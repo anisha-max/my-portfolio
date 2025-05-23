5ec1060e983703a03659ed16a310b49ad92b2883
anisha-max <parmaranisha761@gmail.com>
2025-05-23 22:02:14 +0530

    changes for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout.jsx'
 import Home from './components/Home.jsx'
 import About from './components/About.jsx'
@@ -13,43 +13,22 @@ import ProjectCard from './components/ProjectCard.jsx'
 import Contact from './components/Contact.jsx'
 import GitLoader from './components/GitLoader.js'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <Layout />,
     children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: "home",
-        element: <Home />
-      },
-      {
-        path: "about",
-        element: <About />
-      },
-      {
-        path: "skills",
-        element: <Skill />
-      },
-      {
-        path: "github",
-        element: <Github />,
-        loader: GitLoader
-      },
-      {
-        path: "projects",
-        element: <ProjectCard />
-      },
-      {
-        path: "contact",
-        element: <Contact />
-      }
+      { index: true, element: <Home /> },
+      { path: 'home', element: <Home /> },
+      { path: 'about', element: <About /> },
+      { path: 'skills', element: <Skill /> },
+      { path: 'github', element: <Github />, loader: GitLoader },
+      { path: 'projects', element: <ProjectCard /> },
+      { path: 'contact', element: <Contact /> },
     ]
   }
-])
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
