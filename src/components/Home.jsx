f8e707d282b9a4dbd83daa601a002eafaebbdec3
anisha-max <parmaranisha761@gmail.com>
2025-06-23 21:43:23 +0530

    resume update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,13 +10,6 @@ import Navbar from './Navbar';
 import AchievementsPage from './AchievementsPage';
 
 function Home() {
-  const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = '/AnishaParmar _resume.pdf';
-    link.download = 'Anisha_Resume.pdf';
-    link.click();
-  };
-
   return (
     <>
    
@@ -25,15 +18,15 @@ function Home() {
           <div className='md:ps-48'>
             <h1 className='text-6xl'>Anisha Parmar</h1>
             <p className='italic text-[26px] pt-2 font-bold text-sky-400'>Web Developer</p>
-            <p className=" text-[20px] max-w-xl">
+            <p className=" text-[20px] max-w- mb-4">
               Full Stack Developer passionate about building clean, scalable, and intuitive applications — built with experience, attention to detail, and a commitment to writing efficient, maintainable code. I specialize in the <span className='text-[21px] text-sky-400 font-bold'>MERN</span> stack (MongoDB, Express, React, Node.js) and also leverage <span className='text-[21px] font-bold text-sky-400 bold'>Next.js</span> for building high-performance, server-rendered applications. I focus on delivering high-quality solutions that solve real-world problems and provide great user experiences.
             </p>
-            <button
-              onClick={handleDownload}
+            <a
+              href='AnishaParmarResume.pdf' target='_blank'
               className="px-4 my-4 py-2 rounded text-white bg-gradient-to-r from-sky-500 via-sky-600 to-sky-700 hover:from-sky-600 hover:via-sky-700 hover:to-sky-800 transition duration-300"
             >
-              Download Resume <IoMdDownload className='float-right ms-2 text-xl mt-1' />
-            </button>
+              Download Resume 
+            </a>
           </div>
           <img src="images/developer.png" className='h-[30rem] ms-32 hidden lg:block  hover:scale-105 transition-transform duration-300 ease-in-out '></img>
         </div>
